fe8c0c9afd38547b9bd8e2c88c353ebe0f40dbb6
Kalvin Lam <kalvin.lam5@gmail.com>
2026-01-16 18:55:49 -0500

    matplotlib plots smt

--- explore.py
+++ explore.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv('store-sales-time-series-forecasting/transactions.csv')
 print(df.head())
+
+plt.plot(df['date'], df['transactions'])
+plt.xlabel('Date')
+plt.ylabel('Number of Transactions')
+plt.title('Store Transactions Over Time')
+
+plt.show()
