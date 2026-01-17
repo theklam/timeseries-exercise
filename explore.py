978fd84a841acd9ec1e4378f6edbb61d77e73de7
Kalvin Lam <kalvin.lam5@gmail.com>
2026-01-17 15:19:11 -0500

    filter for past year

--- explore.py
+++ explore.py
@@ -12,6 +12,12 @@ df = df[df['store_nbr'] == 1] # filter for store 1
 print("Number of rows and columns:", df.shape)
 print(df.head())
 
+latest_date = df['date'].max()
+one_year_ago = latest_date - pd.DateOffset(years=1)
+df = df[df['date'] >= one_year_ago]
+print("Number of rows and columns after filtering for last year:", df.shape)
+print(df.head())
+
 plt.plot(df['date'], df['transactions'])
 plt.xlabel('Date')
 plt.ylabel('Number of Transactions')
