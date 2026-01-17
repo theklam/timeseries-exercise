d023e29166364e962951f65fdefd90b61adca60f
Kalvin Lam <kalvin.lam5@gmail.com>
2026-01-17 15:56:42 -0500

    7d rolling avg and check for missing dates

--- explore.py
+++ explore.py
@@ -18,9 +18,19 @@ df = df[df['date'] >= one_year_ago]
 print("Number of rows and columns after filtering for last year:", df.shape)
 print(df.head())
 
-plt.plot(df['date'], df['transactions'])
+# after filtering for store 1 and last year, we check for missing dates
+all_dates = pd.date_range(start=one_year_ago, end=latest_date)
+missing_dates = all_dates.difference(df['date'])
+print("start date:", one_year_ago)
+print("end date:", latest_date)
+print("Missing dates:", missing_dates)
+
+df['transactions_7d_avg'] = df['transactions'].rolling(window=7).mean()
+
+plt.plot(df['date'], df['transactions'], label='Daily')
+plt.plot(df['date'], df['transactions_7d_avg'], label='7-day Average')
 plt.xlabel('Date')
 plt.ylabel('Number of Transactions')
-plt.title('Store Transactions Over Time for Store 1')
+plt.title('Store 1 Transactions (Past Year)')
 
 plt.show()
