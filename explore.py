0c890b46505bab6af1caf18787c5750819c7d373
Kalvin Lam <kalvin.lam5@gmail.com>
2026-01-17 15:12:35 -0500

    filter for store 1

--- explore.py
+++ explore.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 df = pd.read_csv('store-sales-time-series-forecasting/transactions.csv')
+df['date'] = pd.to_datetime(df['date'])
+print("Number of rows and columns:", df.shape)
+print("Types of each column:\n", df.dtypes)
+print(df.head())
+
+df = df[df['store_nbr'] == 1] # filter for store 1
+print("Number of rows and columns:", df.shape)
 print(df.head())
 
 plt.plot(df['date'], df['transactions'])
 plt.xlabel('Date')
 plt.ylabel('Number of Transactions')
-plt.title('Store Transactions Over Time')
+plt.title('Store Transactions Over Time for Store 1')
 
 plt.show()
